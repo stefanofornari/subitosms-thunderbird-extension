cbb6f2f61445b8802f46aa9fb3f7c127ecd029ed
Stefano Fornari <stefano.fornari@gmail.com>
2009-06-27 18:51:00 +0200

    added the list of phones to address the sms

--- src/main/chrome/content/sendsms.js
+++ src/main/chrome/content/sendsms.js
@@ -1,8 +1,33 @@
+function init() {
+    //
+    // I want to display the numbers if this order:
+    //
+    // - mobile
+    // - home
+    // - work
+    //
+    card = window.arguments[1];
+
+    phonelist = document.getElementById("PHONE_LIST");
+    if (!isEmpty(card.cellularNumber)) {
+        phonelist.appendItem(card.cellularNumber, card.cellularNumber, "(M)");
+    }
+    
+    if (!isEmpty(card.homePhone)) {
+        phonelist.appendItem(card.homePhone, card.homePhone, "(H)");
+    }
+    
+    if (!isEmpty(card.workPhone)) {
+        phonelist.appendItem(card.workPhone, card.workPhone, "(W)");
+    }
+
+    phonelist.selectedIndex=0;
+}
+
 function doAccept() {
     var card = window.arguments[1];
 
     if (isDefined(card)) {
-        alert(card.workPhone);
         sendSMS();
     }
 
